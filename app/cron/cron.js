8183b93f054c70f87a0ffd6861156e68c8bac9fe
pomppa <pomppa@tuomas-mbp.local>
2023-02-06 19:25:18 +0200

    tabs to spaces

--- app/cron/cron.js
+++ app/cron/cron.js
@@ -3,16 +3,16 @@ const env = require('../configs/env');
 env.environment();
 
 module.exports = {
-	schedule(client) {
+    schedule(client) {
 
-		/**
+        /**
          * Schedule cron for fridays
          */
-		cron.schedule('0 15 * * FRI', function() {
-			console.log('Running a cron task on ' + new Date());
-			const channel = client.channels.cache.get(process.env.PERJANTAI_CHANNEL_ID);
-			channel.send('https://www.youtube.com/watch?v=76yIAQcmj9Y' + '\n' + 'se ompi periantai ja kohta töipäivä ohitte ;))))),,,räääh');
-		});
-		console.log('Scheduled cronjobs.');
-	},
+        cron.schedule('0 15 * * FRI', function() {
+            console.log('Running a cron task on ' + new Date());
+            const channel = client.channels.cache.get(process.env.PERJANTAI_CHANNEL_ID);
+            channel.send('https://www.youtube.com/watch?v=76yIAQcmj9Y' + '\n' + 'se ompi periantai ja kohta töipäivä ohitte ;))))),,,räääh');
+        });
+        console.log('Scheduled cronjobs.');
+    },
 };
